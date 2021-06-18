f5371b2c6498646b6f7c74196a5b9d4ceeb98718
amaury84 <amaury84@gmail.com>
2021-06-17 22:30:57 -0500

    segunda versión de api para el nivel 2. verificar el post

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def index():
 if __name__ == "__main__":
     app.run()
 """
-
+"""
 from flask import Flask, jsonify
 from flask_restful import Api, Resource, reqparse
 
@@ -29,12 +29,39 @@ def post():
     dna=params["dna"]
     return params
 """
-    print(dna,type(dna))
-    if mutante.isAdn(dna):
-        if mutante.isMutant(dna):
-            return 200
+from flask import Flask, request, jsonify
+from flask_restful import Api, Resource, reqparse
+import json
+
+import mutante
+app = Flask(__name__)
+api = Api(app)
+
+@app.route('/', methods=['GET'])
+def get():
+    return {"mensaje":"** liga de magneto **"},200
+
+@app.route('/mutant', methods=['POST'])
+def post():
+    dna=request.data
+    dna = str(dna)
+    #limpiamos el string antes de hacerlo lista
+    dna = dna.replace("b","")
+    #dna = dna.replace("[","")
+    #dna = dna.replace("]","")
+    dna = dna.replace("\'","")
+    #hacemos el string una lista
+    #dna = dna.split(",")
+    dna = json.loads(dna)
+    #tipo = type(dna["dna"])
+    dnalist=dna["dna"]
+    #return str(dnalist)
+    
+    if mutante.isAdn(dnalist):
+        if mutante.isMutant(dnalist):
+            return "HTTP-OK",200
         else:
-            return 403
-"""
+            return "HTTP-FORBIDEN",403
+
 if __name__ == "__main__":
     app.run()
--- prueba.py
+++ prueba.py
@@ -4,7 +4,7 @@ url = "http://localhost:5000"
 r = requests.get(url)
 print(r.json())
 
-dna = {"dna":["ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"]}
+dna = '{"dna":["ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"]}'
 print(dna,type(dna))
 
 url = "http://localhost:5000/mutant"
